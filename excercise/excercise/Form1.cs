c42db3e4e0a491c05777483240d670c80511b59f
1117262 <1117262@student.hcmus.edu.vn>
2014-05-19 11:24:58 +0700

    sharp
    excercise222

--- excercise/excercise/Form1.cs
+++ excercise/excercise/Form1.cs
@@ -6,15 +6,20 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using excercise.Csharp;
 
-
-namespace WindowsFormsApplication1
+namespace excercise
 {
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Student svnamnhat = new Student();
         }
     }
 }
